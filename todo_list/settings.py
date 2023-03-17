ae13306b3fd0a7ebf1c98ce8aab724029f91271a
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-17 23:41:54 +0500

    install react

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -42,23 +42,31 @@ INSTALLED_APPS = [
     "todos.apps.TodosConfig" ,
 
     #external modules
-    "rest_framework"
+    "rest_framework" ,
+    "corsheaders" 
 ]
 REST_FRAMEWORKS = {
     "DEFAULT_PERMISSION_CLASSES": [
-    'rest_framework.permissions.AllowAny'
+    'rest_framework.permissions.AllowAny' ,
     ]
 }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_WHITELIST = (
+
+    "http://localhost:3000" , 
+    "http://localhost:8000" , 
+)
 
 ROOT_URLCONF = "todo_list.urls"
 
